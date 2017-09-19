fd50d61e696b3888a9e95813ea659ed4e78a283f
MarkPhillips7 <markphillips7@gmail.com>
2017-09-19 11:21:03 -0400

    initial work on email templates

---
+++ private/jsreportonline/quote.js
@@ -0,0 +1,9 @@
+// require('moment')
+//
+// function formatDate(_date) {
+//     return moment(_date).format('MMMM Do YYYY');
+// }
+//
+// function formatDollars(value) {
+//     return '$' + value.toFixed(2).replace(/(\d)(?=(\d{3})+\.)/g, '$1,');
+// }
